ee9393ca065e9143db1d53ce75361cae7dde81f2
Chaosmonkeys <bonhyup.koo@gmail.com>
2020-08-04 23:52:50 +0900

    상수의 선언과 초기화

---
+++ golang/go-example/1/3_const.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main()  {
+	const a int = 1 // 첫번째 값은 무조건 초기화
+	const b, d int = 2, 3
+	const c = "hi there" // 다른 형 선언 가능
+	const 
+		f = 55
+		k = 44
+		u
+		r
+		t = "end"
+	)
+
+	fmt.Println(f, k, u , r, t)
+}
